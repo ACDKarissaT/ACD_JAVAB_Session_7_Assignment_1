bfa9c9c485b3554c34dba053cc5528bf5e3a2e65
karituason <karituason@users.noreply.github.com>
2019-08-15 08:52:54 -0400

    Add files via upload

---
+++ StringConversion.java
@@ -0,0 +1,11 @@
+package assignment1;
+
+public class StringConversion {
+	public static void main(String[] args) {
+		int num = 1234;
+		String s = String.valueOf(num);
+		
+		System.out.println("Number: " + num);
+		System.out.println("String: " + s);
+	}
+}
